292c8d9d5a4fe49b56d7f98c68a5f705409c76b9
syrjfsih <rajzchanel@gmail.com>
2024-11-20 20:26:30 +0700

    syrjfsih

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Home from './components/Home'
+import SideNav from './components/SideNav'
+import Footer from './components/Footer';
+
+function App() {
+  return (
+   <>
+   <Header />
+   <Home />
+   <SideNav />
+   <Footer />
+   </>
+  );
+}
+
+export default App;
